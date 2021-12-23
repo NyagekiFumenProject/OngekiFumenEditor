6440d845262d9c18d05a282a6097b82221d84b81
MikiraSora <mikirasora0409@126.com>
2021-12-23 18:42:26 +0800

    add

---
+++ OngekiFumenEditor/Utils/ComparerWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditor.Utils
+{
+    public class ComparerWrapper<T> : IComparer<T>
+    {
+        private Func<T, T, int> comp;
+
+        public ComparerWrapper(Func<T, T, int> comp) => this.comp = comp;
+
+        public int Compare(T x, T y)
+        {
+            return comp(x, y);
+        }
+    }
+}
