588e5239f5311adc10673738533e4974ea7fcc5b
MikiraSora <mikirasora0409@126.com>
2021-12-30 22:21:55 +0800

    migrate Mouse* and Drop* actions from EditorView to EditorViewModel

--- OngekiFumenEditor/Modules/FumenVisualEditor/ViewModels/FumenVisualEditorViewModel.cs
+++ OngekiFumenEditor/Modules/FumenVisualEditor/ViewModels/FumenVisualEditorViewModel.cs
@@ -1,6 +1,8 @@
 using Caliburn.Micro;
 using Gemini.Framework;
 using Gemini.Framework.Threading;
+using Gemini.Modules.Toolbox;
+using Gemini.Modules.Toolbox.Models;
 using Gemini.Modules.Toolbox.Services;
 using OngekiFumenEditor.Base;
 using OngekiFumenEditor.Base.OngekiObjects;
@@ -29,6 +31,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels
@@ -76,6 +79,8 @@ namespace OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels
         public ObservableCollection<XGridUnitLineViewModel> XGridUnitLineLocations { get; } = new();
         public ObservableCollection<TGridUnitLineViewModel> TGridUnitLineLocations { get; } = new();
         public ItemCollection DisplayObjectList => View?.DisplayObjectList.Items;
+        public bool isDragging;
+        public bool isMouseDown;
 
         private string errorMessage;
         public string ErrorMessage
@@ -418,5 +423,109 @@ namespace OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels
                     propertyBrowser.OngekiObject = default;
             }
         }
+
+        #region DragActions
+
+        public void OnMouseLeave(ActionExecutionContext e)
+        {
+            //Log.LogInfo("OnMouseLeave");
+            if (!(isMouseDown && View.Parent is IInputElement parent))
+                return;
+            isMouseDown = false;
+            isDragging = false;
+            var pos = (e.EventArgs as MouseEventArgs).GetPosition(parent);
+            SelectObjects.ForEach(x => x.OnDragEnd(pos));
+            //e.Handled = true;
+        }
+
+        public void OnMouseUp(ActionExecutionContext e)
+        {
+            //Log.LogInfo("OnMouseUp");
+            if (!(isMouseDown && View.Parent is IInputElement parent))
+                return;
+
+            var pos = (e.EventArgs as MouseEventArgs).GetPosition(parent);
+            if (isDragging)
+                SelectObjects.ForEach(x => x.OnDragEnd(pos));
+
+            isMouseDown = false;
+            isDragging = false;
+            //e.Handled = true;
+        }
+
+        public void OnMouseMove(ActionExecutionContext e)
+        {
+            //Log.LogInfo("OnMouseMove");
+            if (!(isMouseDown && View.Parent is IInputElement parent))
+                return;
+            //e.Handled = true;
+            var r = isDragging;
+            Action<DisplayObjectViewModelBase, Point> dragCall = (vm, pos) =>
+            {
+                if (r)
+                    vm.OnDragEnd(pos);
+                else
+                    vm.OnDragStart(pos);
+            };
+            isDragging = true;
+
+            var pos = (e.EventArgs as MouseEventArgs).GetPosition(parent);
+            if (VisualTreeUtility.FindParent<Visual>(View) is FrameworkElement uiElement)
+            {
+                var bound = new Rect(0, 0, uiElement.ActualWidth, uiElement.ActualHeight);
+                if (bound.Contains(pos))
+                {
+                    SelectObjects.ForEach(x => dragCall(x, pos));
+                }
+            }
+            else
+            {
+                SelectObjects.ForEach(x => dragCall(x, pos));
+            }
+        }
+
+        public void OnMouseDown(ActionExecutionContext e)
+        {
+            //Log.LogInfo("OnMouseDown");
+            if ((e.EventArgs as MouseEventArgs).LeftButton == MouseButtonState.Pressed)
+            {
+                isMouseDown = true;
+                isDragging = false;
+                //e.Handled = true;
+            }
+            View.Focus();
+        }
+
+        public void Grid_DragEnter(ActionExecutionContext e)
+        {
+            var arg = e.EventArgs as DragEventArgs;
+            if (!arg.Data.GetDataPresent(ToolboxDragDrop.DataFormat))
+                arg.Effects = DragDropEffects.None;
+        }
+
+        public void Grid_Drop(ActionExecutionContext e)
+        {
+            var arg = e.EventArgs as DragEventArgs;
+            if (!arg.Data.GetDataPresent(ToolboxDragDrop.DataFormat))
+                return;
+
+            var mousePosition = arg.GetPosition(View.VisualDisplayer);
+            var displayObject = default(DisplayObjectViewModelBase);
+
+            switch (arg.Data.GetData(ToolboxDragDrop.DataFormat))
+            {
+                case ToolboxItem toolboxItem:
+                    displayObject = Activator.CreateInstance(toolboxItem.ItemType) as DisplayObjectViewModelBase;
+                    break;
+                case OngekiObjectDropParam dropParam:
+                    displayObject = dropParam.OngekiObjectViewModel.Value;
+                    break;
+            }
+
+            OnNewObjectAdd(displayObject);
+            displayObject.MoveCanvas(mousePosition);
+        }
+
+        #endregion
     }
 }
