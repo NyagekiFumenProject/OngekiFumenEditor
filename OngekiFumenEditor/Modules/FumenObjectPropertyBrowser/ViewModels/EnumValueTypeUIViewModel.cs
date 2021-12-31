48c11b741f3fbb1c7e5148731f7f0c557eb965a8
MikiraSora <mikirasora0409@126.com>
2021-12-31 13:45:30 +0800

    add EnumValueTypeUIViewModel

---
+++ OngekiFumenEditor/Modules/FumenObjectPropertyBrowser/ViewModels/EnumValueTypeUIViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using OngekiFumenEditor.Modules.FumenObjectPropertyBrowser.UIGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OngekiFumenEditor.Modules.FumenObjectPropertyBrowser.ViewModels
+{
+    public class EnumValueTypeUIViewModel : CommonUIViewModelBase
+    {
+        public IEnumerable<string> EnumValues => Enum.GetNames(PropertyInfo.PropertyInfo.PropertyType);
+
+        public string Value
+        {
+            get => PropertyInfo.ProxyValue?.ToString();
+            set => PropertyInfo.ProxyValue = Enum.Parse(PropertyInfo.PropertyInfo.PropertyType, value);
+        }
+
+        public EnumValueTypeUIViewModel(PropertyInfoWrapper wrapper) : base(wrapper)
+        {
+
+        }
+    }
+}
