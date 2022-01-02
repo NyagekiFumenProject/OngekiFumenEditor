b0c1d7aa0b76db471c2b8b36172d682f4b546925
MikiraSora <mikirasora0409@126.com>
2022-01-02 13:23:43 +0800

    when BPMChange object finished dragging , update ongeki objects.

--- OngekiFumenEditor/Modules/FumenVisualEditor/ViewModels/OngekiObjects/BPMChangeViewModel.cs
+++ OngekiFumenEditor/Modules/FumenVisualEditor/ViewModels/OngekiObjects/BPMChangeViewModel.cs
@@ -3,6 +3,7 @@ using OngekiFumenEditor.Base.OngekiObjects;
 using OngekiFumenEditor.Utils;
 using System.ComponentModel.Composition;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 
@@ -14,5 +15,12 @@ namespace OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels.OngekiObjects
         public override Brush DisplayBrush => Brushes.Pink;
         private static BindingBase SharedDisplayValueBinding = new Binding("ReferenceOngekiObject.BPM");
         public override BindingBase DisplayValueBinding => SharedDisplayValueBinding;
+
+        public override void OnDragEnd(Point pos)
+        {
+            base.OnDragEnd(pos);
+            //when bpmChange drag done. all ongeki object need to redraw to adapt new bpmList changed.
+            EditorViewModel.Redraw(FumenVisualEditorViewModel.RedrawTarget.OngekiObjects);
+        }
     }
 }
