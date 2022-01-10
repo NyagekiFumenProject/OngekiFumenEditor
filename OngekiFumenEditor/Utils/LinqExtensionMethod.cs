c1a7455cfdb3b0b1ae23d9c692c86695c64a129c
MikiraSora <mikirasora0409@126.com>
2022-01-10 23:15:01 +0800

    add new linq functions : FindNextOrDefault()/FindPreviousOrDefault()

--- OngekiFumenEditor/Utils/LinqExtensionMethod.cs
+++ OngekiFumenEditor/Utils/LinqExtensionMethod.cs
@@ -20,5 +20,35 @@ namespace OngekiFumenEditor.Utils
         public static IEnumerable<T> DistinctBy<T>(this IEnumerable<T> collection, Func<T, T, bool> compFunc) => collection.Distinct(new FuncDistinctComparer<T>(compFunc));
 
         public static IEnumerable<T> FilterNull<T>(this IEnumerable<T> collection) => collection.Where(x => x != null);
+
+        public static T FindNextOrDefault<T>(this IEnumerable<T> collection, T taget)
+        {
+            var itor = collection.GetEnumerator();
+            var prev = default(T);
+
+            while (itor.MoveNext())
+            {
+                if (prev.Equals(taget))
+                    return itor.Current;
+                prev = itor.Current;
+            }
+
+            return default;
+        }
+
+        public static T FindPreviousOrDefault<T>(this IEnumerable<T> collection, T taget)
+        {
+            var itor = collection.GetEnumerator();
+            var prev = default(T);
+
+            while (itor.MoveNext())
+            {
+                if (itor.Current.Equals(taget))
+                    return prev;
+                prev = itor.Current;
+            }
+
+            return default;
+        }
     }
 }
