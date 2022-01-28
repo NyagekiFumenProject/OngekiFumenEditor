002527fb0ceb2fd5957269c8baec7461efad0ac0
MikiraSora <mikirasora0409@126.com>
2022-01-28 09:24:02 +0800

    add To****WithObjectPool()

--- OngekiFumenEditor/Utils/LinqExtensionMethod.cs
+++ OngekiFumenEditor/Utils/LinqExtensionMethod.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OngekiFumenEditor.Utils.ObjectPool;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
@@ -53,6 +54,31 @@ namespace OngekiFumenEditor.Utils
             }
 
             return default;
+        }
+
+        public static IDisposable ToListWithObjectPool<T>(this IEnumerable<T> collection, out List<T> list)
+        {
+            var disposable = ObjectPool<List<T>>.GetWithUsingDisposable(out list, out _);
+            list.Clear();
+            list.AddRange(collection);
+            return disposable;
+        }
+
+        public static IDisposable ToHashSetWithObjectPool<T>(this IEnumerable<T> collection, out HashSet<T> set)
+        {
+            var disposable = ObjectPool<HashSet<T>>.GetWithUsingDisposable(out set, out _);
+            set.Clear();
+            set.AddRange(collection);
+            return disposable;
+        }
+
+        public static IDisposable ToDictionaryWithObjectPool<T, K, V>(this IEnumerable<T> collection, Func<T, K> keySelector, Func<T, V> valueSelector, out Dictionary<K, V> dic)
+        {
+            var disposable = ObjectPool<Dictionary<K, V>>.GetWithUsingDisposable(out dic, out _);
+            dic.Clear();
+            foreach (var item in collection)
+                dic[keySelector(item)] = valueSelector(item);
+            return disposable;
         }
     }
 }
