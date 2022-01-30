9b0a700be5776c97f7c42603f123b35c37e9ccdf
MikiraSora <mikirasora0409@126.com>
2022-01-30 20:46:47 +0800

    fix bug

--- OngekiFumenEditor/Base/OngekiObjects/ConnectableObject/ConnectableChildObjectBase.cs
+++ OngekiFumenEditor/Base/OngekiObjects/ConnectableObject/ConnectableChildObjectBase.cs
@@ -8,7 +8,7 @@ namespace OngekiFumenEditor.Base.OngekiObjects.ConnectableObject
 {
     public abstract class ConnectableChildObjectBase : ConnectableObjectBase
     {
-        public override int RecordId { get => ReferenceStartObject.RecordId; set { } }
+        public override int RecordId { get => ReferenceStartObject?.RecordId ?? int.MinValue; set { } }
 
         public override bool CheckVisiable(TGrid minVisibleTGrid, TGrid maxVisibleTGrid)
         {
