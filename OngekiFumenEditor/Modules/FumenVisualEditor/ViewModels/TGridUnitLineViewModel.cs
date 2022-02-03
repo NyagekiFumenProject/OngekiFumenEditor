484168a5a63238ddfe01b27c71412ae304dee09f
MikiraSora <mikirasora0409@126.com>
2022-02-03 16:24:11 +0800

    remove IsBaseLine

--- OngekiFumenEditor/Modules/FumenVisualEditor/ViewModels/TGridUnitLineViewModel.cs
+++ OngekiFumenEditor/Modules/FumenVisualEditor/ViewModels/TGridUnitLineViewModel.cs
@@ -7,8 +7,7 @@ namespace OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels
         public double Y { get; set; }
         public TGrid TGrid { get; set; }
         public double BeatRhythm { get; set; }
-        public bool IsBaseLine { get; set; }
-        public override string ToString() => $"{Y:F4} {TGrid} {(IsBaseLine ? "BaseLine" : string.Empty)}";
+        public override string ToString() => $"{Y:F4} {TGrid}";
     }
 
 }
