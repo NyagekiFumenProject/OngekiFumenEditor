f29f1ce7ef9aef122edcc746082e6471f34e77cf
MikiraSora <mikirasora0409@126.com>
2022-03-04 12:56:43 +0800

    fix crash

--- OngekiFumenEditor/Modules/FumenVisualEditor/Converters/AuxiliaryLineVisibilityConverter.cs
+++ OngekiFumenEditor/Modules/FumenVisualEditor/Converters/AuxiliaryLineVisibilityConverter.cs
@@ -13,7 +13,7 @@ namespace OngekiFumenEditor.Modules.FumenVisualEditor.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return values.All(x => (Visibility)x == Visibility.Visible) ? Visibility.Visible : Visibility.Hidden;
+            return values.OfType<Visibility>().All(x => x == Visibility.Visible) ? Visibility.Visible : Visibility.Hidden;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
