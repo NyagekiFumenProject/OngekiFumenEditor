ff40ba1e3bf4dfcad04a18ac0ce293199fa9c1c7
MikiraSora <mikirasora0409@126.com>
2022-03-19 20:23:51 +0800

    use ObjectPoolItemsControl

--- OngekiFumenEditor/UI/Controls/ObjectPoolItemsControl.cs
+++ OngekiFumenEditor/UI/Controls/ObjectPoolItemsControl.cs
@@ -12,16 +12,39 @@ namespace OngekiFumenEditor.UI.Controls
 {
     public class ObjectPoolItemsControl : ItemsControl
     {
-        protected override void ClearContainerForItemOverride(DependencyObject element, object item)
+        Dictionary<Type, List<DependencyObject>> cachedObject = new();
+        private object dirtyPrepareCreateItem = default;
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
         {
-            base.ClearContainerForItemOverride(element, item);
-            Log.LogDebug($"{element} {item}");
+            dirtyPrepareCreateItem = item;
+            return base.IsItemItsOwnContainerOverride(item);
         }
 
-        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        protected override DependencyObject GetContainerForItemOverride()
         {
-            base.PrepareContainerForItemOverride(element, item);
-            Log.LogDebug($"{element} {item}");
+            if (cachedObject.TryGetValue(dirtyPrepareCreateItem.GetType(), out var list) && list.Count > 0)
+            {
+                var obj = list[0];
+                list.Remove(obj);
+                //Log.LogDebug($"add {dirtyPrepareCreateItem.GetType().Name} -> ({(obj as ContentPresenter)?.Content?.GetType()?.Name})");
+                return obj;
+            }
+            return base.GetContainerForItemOverride();
         }
+
+        protected override void ClearContainerForItemOverride(DependencyObject element, object item)
+        {
+            var type = item.GetType();
+            //Log.LogDebug($"remove {type.Name} -> ({(element as ContentPresenter)?.Content?.GetType()?.Name})");
+            if (!cachedObject.TryGetValue(type, out var list))
+            {
+                list = new();
+                cachedObject[type] = list;
+            }
+            list.Add(element);
+        }
+
+        public void ClearCacheViews() => cachedObject.Clear();
     }
 }
