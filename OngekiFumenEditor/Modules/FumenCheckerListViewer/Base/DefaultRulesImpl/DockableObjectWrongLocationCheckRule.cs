0a09d6e86b240da9fe19c2081592652db32d4dab
MikiraSora <mikirasora0409@126.com>
2022-04-05 16:56:42 +0800

    add DockableObjectWrongLocationCheckRule and MissingRefObjectCheckRule

---
+++ OngekiFumenEditor/Modules/FumenCheckerListViewer/Base/DefaultRulesImpl/DockableObjectWrongLocationCheckRule.cs
@@ -0,0 +1,50 @@
+﻿using OngekiFumenEditor.Base;
+using OngekiFumenEditor.Base.OngekiObjects.ConnectableObject;
+using OngekiFumenEditor.Base.OngekiObjects.Lane.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditor.Modules.FumenCheckerListViewer.Base.DefaultRulesImpl
+{
+    [Export(typeof(IFumenCheckRule))]
+    internal class DockableObjectWrongLocationCheckRule : IFumenCheckRule
+    {
+        const string RuleName = "WrongLocation";
+
+        public IEnumerable<ICheckResult> CheckRule(OngekiFumen fumen, object fumenHostViewModel)
+        {
+            foreach (var dockableObj in fumen.Holds
+                .AsEnumerable<ILaneDockable>()
+                .Concat(fumen.Taps)
+                .Where(x => x.ReferenceLaneStart is not null)
+                .Where(x => CheckIsWrongLocation(x, x.ReferenceLaneStart))
+                )
+            {
+                yield return new CommonCheckResult()
+                {
+                    Description = $"{dockableObj.GetType().Name}物件貌似没有放置在正确的轨道(id:{dockableObj.ReferenceLaneStrId})线上",
+                    LocationDescription = dockableObj.ToString(),
+                    NavigateTGridLocation = dockableObj.TGrid,
+                    RuleName = RuleName,
+                    Severity = RuleSeverity.Problem
+                };
+            }
+        }
+
+        private bool CheckIsWrongLocation(ILaneDockable x, LaneStartBase referenceLaneStart)
+        {
+            if (x.TGrid > referenceLaneStart.MaxTGrid || x.TGrid < referenceLaneStart.MinTGrid)
+                return true;
+
+            var calXGrid = referenceLaneStart.CalulateXGrid(x.TGrid);
+            if (calXGrid is null)
+                return false;
+
+            return Math.Abs((calXGrid - x.XGrid).TotalGrid(calXGrid.ResX)) > calXGrid.ResX * 1;
+        }
+    }
+}
---
+++ OngekiFumenEditor/Modules/FumenCheckerListViewer/Base/DefaultRulesImpl/MissingRefObjectCheckRule.cs
@@ -0,0 +1,44 @@
+﻿using OngekiFumenEditor.Base;
+using OngekiFumenEditor.Base.OngekiObjects.ConnectableObject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditor.Modules.FumenCheckerListViewer.Base.DefaultRulesImpl
+{
+    [Export(typeof(IFumenCheckRule))]
+    internal class MissingRefObjectCheckRule : IFumenCheckRule
+    {
+        const string RuleName = "MissingRefObject";
+
+        public IEnumerable<ICheckResult> CheckRule(OngekiFumen fumen, object fumenHostViewModel)
+        {
+            foreach (var dockableObj in fumen.Holds.AsEnumerable<ILaneDockable>().Concat(fumen.Taps).Where(x => x.ReferenceLaneStart is null))
+            {
+                yield return new CommonCheckResult()
+                {
+                    Description = $"{dockableObj.GetType().Name}物件缺少引用Lane物件",
+                    LocationDescription = dockableObj.ToString(),
+                    NavigateTGridLocation = dockableObj.TGrid,
+                    RuleName = RuleName,
+                    Severity = RuleSeverity.Error
+                };
+            }
+
+            foreach (var dockableObj in fumen.Holds.AsEnumerable<ILaneDockable>().Concat(fumen.Taps).Where(x => !fumen.Lanes.Contains(x.ReferenceLaneStart)))
+            {
+                yield return new CommonCheckResult()
+                {
+                    Description = $"{dockableObj.GetType().Name}物件引用的Lane物件(laneId:{dockableObj.ReferenceLaneStrId}),不存在于谱面文件内",
+                    LocationDescription = dockableObj.ToString(),
+                    NavigateTGridLocation = dockableObj.TGrid,
+                    RuleName = RuleName,
+                    Severity = RuleSeverity.Error
+                };
+            }
+        }
+    }
+}
