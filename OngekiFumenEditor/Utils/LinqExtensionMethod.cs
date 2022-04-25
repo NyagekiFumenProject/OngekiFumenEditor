6c80015155a4642840e9dc4fedd0cd501931012d
MikiraSora <mikirasora0409@126.com>
2022-04-25 10:37:32 +0800

    add new linq DistinctContinuousBy()

--- OngekiFumenEditor/Utils/LinqExtensionMethod.cs
+++ OngekiFumenEditor/Utils/LinqExtensionMethod.cs
@@ -31,6 +31,44 @@ namespace OngekiFumenEditor.Utils
         public static IEnumerable<T> DistinctBy<T, Y>(this IEnumerable<T> collection, Func<T, Y> keySelect) => collection.DistinctBy((a, b) => keySelect(a)?.Equals(keySelect(b)) ?? keySelect(b)?.Equals(keySelect(a)) ?? true);
 
         public static IEnumerable<T> DistinctBy<T>(this IEnumerable<T> collection, Func<T, T, bool> compFunc) => collection.Distinct(new FuncDistinctComparer<T>(compFunc));
+        
+        /// <summary>
+        /// Distinct continuous same values.
+        /// example : 1,2,3,3,4,1,1,2,3 -> 1,2,3,4,1,2,3
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="compFunc"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> DistinctContinuousBy<T, Y>(this IEnumerable<T> collection, Func<T, Y> keySelect) => collection.DistinctContinuousBy((a, b) => keySelect(a)?.Equals(keySelect(b)) ?? keySelect(b)?.Equals(keySelect(a)) ?? true);
+        
+        /// <summary>
+        /// Distinct continuous same values.
+        /// example : 1,2,3,3,4,1,1,2,3 -> 1,2,3,4,1,2,3
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="compFunc"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> DistinctContinuousBy<T>(this IEnumerable<T> collection, Func<T, T, bool> compFunc)
+        {
+            var itor = collection.GetEnumerator();
+            bool isFirst = true;
+            var prev = default(T);
+
+            while (itor.MoveNext())
+            {
+                var value = itor.Current;
+                if (isFirst)
+                {
+                    yield return value;
+                    isFirst = false;
+                }
+                else if (!compFunc(prev, value))
+                        yield return value;
+                prev = value;
+            }
+        }
 
         public static IEnumerable<T> FilterNull<T>(this IEnumerable<T> collection) => collection.Where(x => x != null);
 
@@ -288,7 +326,7 @@ namespace OngekiFumenEditor.Utils
         public static bool IsOnlyOne<T>(this IEnumerable<T> a)
             => IsOnlyOne(a, out _);
 
-        public static bool IsOnlyOne<T>(this IEnumerable<T> a,out T firstElement)
+        public static bool IsOnlyOne<T>(this IEnumerable<T> a, out T firstElement)
         {
             firstElement = default;
 
