696a7936d9f9cfb82a5e6f74855b17b7033ee413
MikiraSora <mikirasora0409@126.com>
2022-05-07 03:00:45 +0800

    add hotkey

--- OngekiFumenEditor/Modules/FumenVisualEditor/Commands/BrushModeSwitch/BrushModeSwitchCommandDefinition.cs
+++ OngekiFumenEditor/Modules/FumenVisualEditor/Commands/BrushModeSwitch/BrushModeSwitchCommandDefinition.cs
@@ -1,9 +1,11 @@
 ﻿using Gemini.Framework.Commands;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OngekiFumenEditor.Modules.FumenVisualEditor.Commands.BrushModeSwitch
 {
@@ -17,5 +19,8 @@ namespace OngekiFumenEditor.Modules.FumenVisualEditor.Commands.BrushModeSwitch
         public override string ToolTip => "如果开启笔刷模式,避免大部分物件拖拉操作";
 
         public override Uri IconSource => new Uri("pack://application:,,,/OngekiFumenEditor;component/Resources/Icons/icons8-paint-brush-16.png");
+
+        [Export]
+        public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<BrushModeSwitchCommandDefinition>(new (Key.B, ModifierKeys.Alt));
     }
 }
