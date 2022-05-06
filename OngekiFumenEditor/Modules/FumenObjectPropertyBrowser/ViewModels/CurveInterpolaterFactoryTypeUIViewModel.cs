ef8ab4614e27a1bbcf8792f6a865b589d314b599
MikiraSora <mikirasora0409@126.com>
2022-05-07 01:14:49 +0800

    fix bug

--- OngekiFumenEditor/Modules/FumenObjectPropertyBrowser/ViewModels/CurveInterpolaterFactoryTypeUIViewModel.cs
+++ OngekiFumenEditor/Modules/FumenObjectPropertyBrowser/ViewModels/CurveInterpolaterFactoryTypeUIViewModel.cs
@@ -14,43 +14,11 @@ namespace OngekiFumenEditor.Modules.FumenObjectPropertyBrowser.ViewModels
 {
     public class CurveInterpolaterFactoryTypeUIViewModel : CommonUIViewModelBase<ICurveInterpolaterFactory>
     {
-        public IEnumerable<string> EnumValues => IoC.GetAll<ICurveInterpolaterFactory>().Select(x => x.Name);
-
-        public string FactoryName
-        {
-            get => TypedProxyValue?.Name;
-            set
-            {
-                var def = DefaultCurveInterpolaterFactory.Default;
-                if (PropertyInfo is UndoablePropertyInfoWrapper undoable)
-                    undoable.ExecuteSubPropertySetAction(nameof(ConnectableChildObjectBase.CurveInterpolaterFactory), (val) =>
-                    {
-                        var factory = IoC.GetAll<ICurveInterpolaterFactory>().FirstOrDefault(x => x.Name == val) ?? def;
-                        TypedProxyValue = factory;
-                    }, FactoryName, value);
-                else
-                {
-                    TypedProxyValue = IoC.GetAll<ICurveInterpolaterFactory>().FirstOrDefault(x => x.Name == value) ?? def;
-                }
-
-                NotifyOfPropertyChange(() => FactoryName);
-            }
-        }
+        public IEnumerable<ICurveInterpolaterFactory> EnumValues => IoC.GetAll<ICurveInterpolaterFactory>();
 
         public CurveInterpolaterFactoryTypeUIViewModel(PropertyInfoWrapper wrapper) : base(wrapper)
         {
-            if (PropertyInfo.OwnerObject is INotifyPropertyChanged notifyProperty)
-            {
-                notifyProperty.PropertyChanged += NotifyProperty_PropertyChanged;
-            }
-        }
 
-        private void NotifyProperty_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(ConnectableChildObjectBase.CurveInterpolaterFactory))
-            {
-                Refresh();
-            }
         }
     }
 }
