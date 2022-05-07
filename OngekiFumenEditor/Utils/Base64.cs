84cf4347a595ce59dd55604e9f9da55a1d730fc1
MikiraSora <mikirasora0409@126.com>
2022-05-08 03:48:15 +0800

    add Base64

---
+++ OngekiFumenEditor/Utils/Base64.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditor.Utils
+{
+    public static class Base64
+    {
+        public static string Decode(string base64Content, Encoding encoding) => encoding.GetString(Convert.FromBase64String(base64Content));
+        public static string Decode(string base64Content) => Decode(base64Content, Encoding.UTF8);
+
+        public static string Encode(string rawContent, Encoding encoding) => Convert.ToBase64String(encoding.GetBytes(rawContent));
+        public static string Encode(string rawContent) => Encode(rawContent, Encoding.UTF8);
+    }
+}
