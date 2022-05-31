7fbd907798329d05b47dbff34ce071b8ca526cbf
MikiraSora <mikirasora0409@126.com>
2022-05-31 17:22:20 +0800

    add FastPickLane

---
+++ OngekiFumenEditor/Modules/FumenVisualEditor/Commands/FastPickLane/FastPickLaneCommandDefinition.cs
@@ -0,0 +1,42 @@
+﻿using Gemini.Framework.Commands;
+using OngekiFumenEditor.Base.OngekiObjects.Lane;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OngekiFumenEditor.Modules.FumenVisualEditor.Commands.FastPickLane
+{
+    public abstract class FastPickLaneCommandDefinition<T> : CommandDefinition
+    {
+        public override string Name => $"Editor.FastPickLane_{typeof(T).Name}";
+
+        public override string Text => $"编辑器快速选择轨道({typeof(T).Name})";
+
+        public override string ToolTip => Text;
+    }
+
+    [CommandDefinition]
+    public class FastPickRightLaneCommandDefinition : FastPickLaneCommandDefinition<LaneRightStart>
+    {
+        [Export]
+        public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<FastPickRightLaneCommandDefinition>(new(Key.V, ModifierKeys.Alt));
+    }
+
+    [CommandDefinition]
+    public class FastPickCenterLaneCommandDefinition : FastPickLaneCommandDefinition<LaneCenterStart>
+    {
+        [Export]
+        public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<FastPickCenterLaneCommandDefinition>(new(Key.C, ModifierKeys.Alt));
+    }
+
+    [CommandDefinition]
+    public class FastPickLeftLaneCommandDefinition : FastPickLaneCommandDefinition<LaneLeftStart>
+    {
+        [Export]
+        public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<FastPickLeftLaneCommandDefinition>(new(Key.X, ModifierKeys.Alt));
+    }
+}
---
+++ OngekiFumenEditor/Modules/FumenVisualEditor/Commands/FastPickLane/FastPickLaneCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Gemini.Framework.Commands;
+using Gemini.Framework.Services;
+using Gemini.Framework.Threading;
+using OngekiFumenEditor.Base.OngekiObjects.ConnectableObject;
+using OngekiFumenEditor.Base.OngekiObjects.Lane;
+using OngekiFumenEditor.Base.OngekiObjects.Lane.Base;
+using OngekiFumenEditor.Modules.FumenVisualEditor.Kernel;
+using OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels;
+using OngekiFumenEditor.Utils;
+
+namespace OngekiFumenEditor.Modules.FumenVisualEditor.Commands.FastPickLane
+{
+    public abstract class FastPickLaneCommandHandler<T, DEF> : CommandHandlerBase<DEF> where DEF : FastPickLaneCommandDefinition<T> where T : LaneStartBase
+    {
+        public override Task Run(Command command)
+        {
+            if (IoC.Get<IEditorDocumentManager>().CurrentActivatedEditor is not FumenVisualEditorViewModel editor)
+                return TaskUtility.Completed;
+
+            var filterTGrid = TGridCalculator.ConvertYToTGrid(editor.MaxVisibleCanvasY, editor);
+            var selectLane = editor.Fumen.Lanes.OfType<T>().Where(x => x.MaxTGrid <= filterTGrid).OrderBy(x => x.MaxTGrid).LastOrDefault();
+
+            var obj = selectLane?.Children.LastOrDefault() as ConnectableObjectBase;
+            obj = obj ?? selectLane;
+
+            if (obj is not null)
+                editor.NotifyObjectClicked(obj);
+
+            return TaskUtility.Completed;
+        }
+    }
+
+    [CommandHandler]
+    public class FastPickRightLaneCommandHandler : FastPickLaneCommandHandler<LaneRightStart, FastPickRightLaneCommandDefinition>
+    {}
+
+
+    [CommandHandler]
+    public class FastPickCenterLaneCommandHandler : FastPickLaneCommandHandler<LaneCenterStart, FastPickCenterLaneCommandDefinition>
+    { }
+
+
+    [CommandHandler]
+    public class FastPickLeftLaneCommandHandler : FastPickLaneCommandHandler<LaneLeftStart, FastPickLeftLaneCommandDefinition>
+    { }
+}
