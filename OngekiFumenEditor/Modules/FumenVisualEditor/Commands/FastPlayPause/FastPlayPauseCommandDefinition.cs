080ee963ad114433dde47041f6c5337af1eb8220
MikiraSora <mikirasora0409@126.com>
2022-05-31 17:23:04 +0800

    add FastPlayPause

---
+++ OngekiFumenEditor/Modules/FumenVisualEditor/Commands/FastPlayPause/FastPlayPauseCommandDefinition.cs
@@ -0,0 +1,24 @@
+﻿using Gemini.Framework.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OngekiFumenEditor.Modules.FumenVisualEditor.Commands.FastPlayPause
+{
+    [CommandDefinition]
+    public class FastPlayPauseCommandDefinition : CommandDefinition
+    {
+        public override string Name => "Editor.FastPlayPause";
+
+        public override string Text => "编辑器快速播放暂停";
+
+        public override string ToolTip => Text;
+
+        [Export]
+        public static CommandKeyboardShortcut KeyGesture = new CommandKeyboardShortcut<FastPlayPauseCommandDefinition>(new (Key.Space));
+    }
+}
---
+++ OngekiFumenEditor/Modules/FumenVisualEditor/Commands/FastPlayPause/FastPlayPauseCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using Gemini.Framework.Commands;
+using Gemini.Framework.Services;
+using Gemini.Framework.Threading;
+using OngekiFumenEditor.Modules.FumenVisualEditor.Kernel;
+using OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels;
+
+namespace OngekiFumenEditor.Modules.FumenVisualEditor.Commands.FastPlayPause
+{
+    [CommandHandler]
+    public class FastPlayPauseCommandHandler : CommandHandlerBase<FastPlayPauseCommandDefinition>
+    {
+        private IEditorDocumentManager editorDocumentManager;
+
+        [ImportingConstructor]
+        public FastPlayPauseCommandHandler(IEditorDocumentManager editorDocumentManager)
+        {
+            this.editorDocumentManager = editorDocumentManager;
+        }
+
+        public override Task Run(Command command)
+        {
+            editorDocumentManager.CurrentActivatedEditor.KeyboardAction_PlayOrPause();
+            return TaskUtility.Completed;
+        }
+    }
+}
