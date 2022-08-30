bf6d144b9dda0f5d31491559210005cfa828b84e
MikiraSora <mikirasora0409@126.com>
2022-08-30 17:37:33 +0800

    add Task.WithTimeout()

---
+++ OngekiFumenEditor/Utils/TaskExtensionMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditor.Utils
+{
+    public static class TaskExtensionMethod
+    {
+        /// <summary>
+        /// 对一个Task钦定一个timeout，超时就不管了
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="timeoutMsec"></param>
+        /// <returns></returns>
+        public static async Task<T> WithTimeout<T>(this Task<T> t, int timeoutMsec, CancellationToken cancellationToken = default)
+        {
+            var dt = Task.Delay(timeoutMsec, cancellationToken).ContinueWith((a, b) => default(T), cancellationToken);
+            var task = await Task.WhenAny(dt, t);
+
+            return task == t ? await t : default(T);
+        }
+    }
+}
