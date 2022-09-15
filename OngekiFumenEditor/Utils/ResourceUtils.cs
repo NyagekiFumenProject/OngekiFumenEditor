3c7610cd92f934df5c278a07f51801b751771bad
MikiraSora <mikirasora0409@126.com>
2022-09-15 20:37:31 +0800

    ResourceUtils add OpenReadTextureFromResource() and OpenReadResourceStream()

--- OngekiFumenEditor/Utils/ResourceUtils.cs
+++ OngekiFumenEditor/Utils/ResourceUtils.cs
@@ -1,14 +1,31 @@
+using OngekiFumenEditor.Modules.FumenPreviewer.Graphics;
+using OngekiFumenEditor.Modules.FumenPreviewer.Graphics.Drawing.DefaultDrawingImpl.StringDrawing.String;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Texture = OngekiFumenEditor.Modules.FumenPreviewer.Graphics.Texture;
 
 namespace OngekiFumenEditor.Utils
 {
     public static class ResourceUtils
     {
         public static Stream OpenReadFromLocalAssemblyResource(string resourceName) => typeof(ResourceUtils).Assembly.GetManifestResourceStream("OngekiFumenEditor.Resources." + resourceName);
+    
+        public static Stream OpenReadResourceStream(string relativeUrl)
+        {
+            var info = System.Windows.Application.GetResourceStream(new Uri(relativeUrl, UriKind.Relative));
+            return info.Stream;
+        }
+
+        public static Texture OpenReadTextureFromResource(string relativeUrl)
+        {
+            using var stream = OpenReadResourceStream(relativeUrl);
+            using var bitmap = Image.FromStream(stream) as Bitmap;
+            return new Texture(bitmap);
+        }
     }
 }
