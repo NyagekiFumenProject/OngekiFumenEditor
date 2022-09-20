7b4d12d63f0015fed60dbca97a0437a08d471e57
MikiraSora <mikirasora0409@126.com>
2022-09-20 14:17:52 +0800

    support provide a VBO to draw static objects.

--- OngekiFumenEditor/Modules/FumenPreviewer/Graphics/Drawing/DefaultDrawingImpl/LineDrawing/DefaultInstancedLineDrawing.cs
+++ OngekiFumenEditor/Modules/FumenPreviewer/Graphics/Drawing/DefaultDrawingImpl/LineDrawing/DefaultInstancedLineDrawing.cs
@@ -3,9 +3,12 @@ Code copied and modified from https://github.com/mhalber/Lines/blob/master/insta
 */
 
 using Caliburn.Micro;
+using OngekiFumenEditor.Utils;
+using OngekiFumenEditor.Utils.ObjectPool;
 using OpenTK.Graphics.OpenGL;
 using SharpVectors.Dom.Svg;
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
@@ -84,25 +87,9 @@ namespace OngekiFumenEditor.Modules.FumenPreviewer.Graphics.Drawing.DefaultDrawi
                 GL.BindBuffer(BufferTarget.ArrayBuffer, line_vbo);
                 {
                     GL.NamedBufferStorage(line_vbo, PostData.Length * sizeof(float), IntPtr.Zero, BufferStorageFlags.DynamicStorageBit);
+                    SwitchVertexVBO(line_vbo);
                 }
-                GL.VertexArrayVertexBuffer(vao, binding_idx, line_vbo, IntPtr.Zero, VertexTBytesSize);
-                GL.VertexArrayBindingDivisor(vao, binding_idx, 1);
-
-                GL.EnableVertexArrayAttrib(vao, line_pos_width_a);
-                GL.VertexArrayAttribFormat(vao, line_pos_width_a, 4, VertexAttribType.Float, false, 0);
-                GL.VertexArrayAttribBinding(vao, line_pos_width_a, binding_idx);
-
-                GL.EnableVertexArrayAttrib(vao, line_col_a);
-                GL.VertexArrayAttribFormat(vao, line_col_a, 4, VertexAttribType.Float, false, 4 * sizeof(float));
-                GL.VertexArrayAttribBinding(vao, line_col_a, binding_idx);
-
-                GL.EnableVertexArrayAttrib(vao, line_pos_width_b);
-                GL.VertexArrayAttribFormat(vao, line_pos_width_b, 4, VertexAttribType.Float, false, VertexTBytesSize);
-                GL.VertexArrayAttribBinding(vao, line_pos_width_b, binding_idx);
-
-                GL.EnableVertexArrayAttrib(vao, line_col_b);
-                GL.VertexArrayAttribFormat(vao, line_col_b, 4, VertexAttribType.Float, false, VertexTBytesSize + 4 * sizeof(float));
-                GL.VertexArrayAttribBinding(vao, line_col_b, binding_idx);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
 
                 binding_idx++;
 
@@ -134,6 +121,7 @@ namespace OngekiFumenEditor.Modules.FumenPreviewer.Graphics.Drawing.DefaultDrawi
                 GL.VertexArrayAttribBinding(vao, quad_pos, binding_idx);
             }
             GL.BindVertexArray(0);
+
         }
 
         private void FlushDraw()
@@ -145,8 +133,6 @@ namespace OngekiFumenEditor.Modules.FumenPreviewer.Graphics.Drawing.DefaultDrawi
                 GL.DrawElementsInstanced(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedShort, IntPtr.Zero, postDataFillCount - 1);
                 perfomenceMonitor.CountDrawCall(this);
             }
-
-            postDataFillIndex = postDataFillCount = 0;
         }
 
         public void Dispose()
@@ -157,22 +143,6 @@ namespace OngekiFumenEditor.Modules.FumenPreviewer.Graphics.Drawing.DefaultDrawi
             GL.DeleteBuffer(quad_vbo);
             GL.DeleteBuffer(quad_ebo);
             GL.DeleteVertexArray(vao);
-        }
-
-        public void Begin(IFumenPreviewer target, float lineWidth)
-        {
-            perfomenceMonitor.OnBeginDrawing(this);
-            this.target = target;
-            this.lineWidth = lineWidth;
-
-
-            shader.Begin();
-            GL.BindVertexArray(vao);
-            var mvpMatrix = GetOverrideModelMatrix() * target.ViewProjectionMatrix;
-            shader.PassUniform(mvp, mvpMatrix);
-            shader.PassUniform(viewport_size, new OpenTK.Mathematics.Vector2(target.ViewWidth, target.ViewHeight));
-            shader.PassUniform(aa_radius, new OpenTK.Mathematics.Vector2(2, 2));
-
         }
 
         private void PostPointInternal(Vector2 Point, Vector4 Color)
@@ -218,12 +188,117 @@ namespace OngekiFumenEditor.Modules.FumenPreviewer.Graphics.Drawing.DefaultDrawi
             perfomenceMonitor.OnAfterDrawing(this);
         }
 
+        public void Begin(IFumenPreviewer target, float lineWidth)
+        {
+            perfomenceMonitor.OnBeginDrawing(this);
+            this.target = target;
+            this.lineWidth = lineWidth;
+            postDataFillIndex = postDataFillCount = 0;
+
+            shader.Begin();
+            GL.BindVertexArray(vao);
+            var mvpMatrix = GetOverrideModelMatrix() * target.ViewProjectionMatrix;
+            shader.PassUniform(mvp, mvpMatrix);
+            shader.PassUniform(viewport_size, new OpenTK.Mathematics.Vector2(target.ViewWidth, target.ViewHeight));
+            shader.PassUniform(aa_radius, new OpenTK.Mathematics.Vector2(2, 2));
+        }
+
+
         public void Draw(IFumenPreviewer target, IEnumerable<LineVertex> points, float lineWidth)
         {
             Begin(target, lineWidth);
             foreach (var point in points)
                 PostPoint(point.Point, point.Color);
             End();
+        }
+
+        private class VBOHandle : ISimpleLineDrawing.IVBOHandle
+        {
+            public int vbo = int.MinValue;
+            public int verticsCount = 0;
+
+            public void Dispose()
+            {
+                if (vbo == int.MinValue)
+                    return;
+                GL.DeleteBuffer(vbo);
+                vbo = int.MinValue;
+                verticsCount = default;
+            }
+        }
+
+        public void DrawVBO(IFumenPreviewer target, ISimpleLineDrawing.IVBOHandle h)
+        {
+            Begin(target, 1);
+            {
+                var handle = (VBOHandle)h;
+
+                SwitchVertexVBO(handle.vbo);
+
+                GL.DrawElementsInstanced(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedShort, IntPtr.Zero, handle.verticsCount - 1);
+                perfomenceMonitor.CountDrawCall(this);
+
+                SwitchVertexVBO(line_vbo);
+            }
+            End();
+        }
+
+        private void SwitchVertexVBO(int vbo)
+        {
+            GL.VertexArrayVertexBuffer(vao, 0, vbo, IntPtr.Zero, VertexTBytesSize);
+            GL.VertexArrayBindingDivisor(vao, 0, 1);
+
+            GL.EnableVertexArrayAttrib(vao, line_pos_width_a);
+            GL.VertexArrayAttribFormat(vao, line_pos_width_a, 4, VertexAttribType.Float, false, 0);
+            GL.VertexArrayAttribBinding(vao, line_pos_width_a, 0);
+
+            GL.EnableVertexArrayAttrib(vao, line_col_a);
+            GL.VertexArrayAttribFormat(vao, line_col_a, 4, VertexAttribType.Float, false, 4 * sizeof(float));
+            GL.VertexArrayAttribBinding(vao, line_col_a, 0);
+
+            GL.EnableVertexArrayAttrib(vao, line_pos_width_b);
+            GL.VertexArrayAttribFormat(vao, line_pos_width_b, 4, VertexAttribType.Float, false, VertexTBytesSize);
+            GL.VertexArrayAttribBinding(vao, line_pos_width_b, 0);
+
+            GL.EnableVertexArrayAttrib(vao, line_col_b);
+            GL.VertexArrayAttribFormat(vao, line_col_b, 4, VertexAttribType.Float, false, VertexTBytesSize + 4 * sizeof(float));
+            GL.VertexArrayAttribBinding(vao, line_col_b, 0);
+        }
+
+        public ISimpleLineDrawing.IVBOHandle GenerateVBOWithPresetPoints(IEnumerable<LineVertex> vertices, float lineWidth)
+        {
+            var handle = new VBOHandle()
+            {
+                vbo = GL.GenBuffer()
+            };
+            var vbo = handle.vbo;
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            {
+                using var d = vertices.ToListWithObjectPool(out var list);
+                var buffer = ArrayPool<float>.Shared.Rent(list.Count * VertexTBytesSize / sizeof(float));
+                var i = 0;
+
+                foreach (var vertex in vertices)
+                {
+                    buffer[i++] = vertex.Point.X;
+                    buffer[i++] = vertex.Point.Y;
+                    buffer[i++] = 0;
+
+                    buffer[i++] = lineWidth;
+
+                    buffer[i++] = vertex.Color.X;
+                    buffer[i++] = vertex.Color.Y;
+                    buffer[i++] = vertex.Color.Z;
+                    buffer[i++] = vertex.Color.W;
+                }
+
+                handle.verticsCount = list.Count;
+
+                GL.NamedBufferData(vbo, i * sizeof(float), buffer, BufferUsageHint.StaticDraw);
+                ArrayPool<float>.Shared.Return(buffer);
+            }
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            return handle;
         }
     }
 }
--- OngekiFumenEditor/Modules/FumenPreviewer/Graphics/Drawing/ISimpleLineDrawing.cs
+++ OngekiFumenEditor/Modules/FumenPreviewer/Graphics/Drawing/ISimpleLineDrawing.cs
@@ -10,8 +10,16 @@ namespace OngekiFumenEditor.Modules.FumenPreviewer.Graphics.Drawing
 {
     public interface ISimpleLineDrawing : ILineDrawing
     {
+        public interface IVBOHandle : IDisposable
+        {
+
+        }
+
         void Begin(IFumenPreviewer target, float lineWidth);
         void PostPoint(Vector2 Point, Vector4 Color);
         void End();
+
+        void DrawVBO(IFumenPreviewer target, IVBOHandle vbo);
+        IVBOHandle GenerateVBOWithPresetPoints(IEnumerable<LineVertex> points, float lineWidth);
     }
 }
