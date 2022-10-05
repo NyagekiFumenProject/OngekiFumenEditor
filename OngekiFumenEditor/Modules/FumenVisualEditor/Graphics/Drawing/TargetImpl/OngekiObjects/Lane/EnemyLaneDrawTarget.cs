262d80601e4598cc206e4e326ace0bd49f8ef548
MikiraSora <mikirasora0409@126.com>
2022-10-05 13:13:25 +0800

    support enemy lane drawing

---
+++ OngekiFumenEditor/Modules/FumenVisualEditor/Graphics/Drawing/TargetImpl/OngekiObjects/Lane/EnemyLaneDrawTarget.cs
@@ -0,0 +1,20 @@
+﻿using OngekiFumenEditor.Base;
+using OngekiFumenEditor.Base.OngekiObjects.ConnectableObject;
+using OngekiFumenEditor.Base.OngekiObjects.Lane;
+using OngekiFumenEditor.Base.OngekiObjects.Lane.Base;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Numerics;
+
+namespace OngekiFumenEditor.Modules.FumenVisualEditor.Graphics.Drawing.TargetImpl.Lane
+{
+    [Export(typeof(IDrawingTarget))]
+    public class EnemyLaneDrawTarget : NormalLaneDrawingTarget
+    {
+        public static Vector4 LaneColor { get; } = new(1, 1, 0, 1);
+
+        public override Vector4 GetLanePointColor(ConnectableObjectBase obj) => LaneColor;
+
+        public override IEnumerable<string> DrawTargetID { get; } = new[] { "ENS" };
+    }
+}
