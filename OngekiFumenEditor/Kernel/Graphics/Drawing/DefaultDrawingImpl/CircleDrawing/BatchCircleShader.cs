4468299a50968bebe5229543a53aa29dca00f778
MikiraSora <mikirasora0409@126.com>
2023-06-25 17:21:48 +0800

    implement circle(dot) drawing

---
+++ OngekiFumenEditor/Kernel/Graphics/Drawing/DefaultDrawingImpl/CircleDrawing/BatchCircleShader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OngekiFumenEditor.Kernel.Graphics.Base;
+using OngekiFumenEditor.Kernel.Graphics.Drawing.DefaultDrawingImpl.TextureDrawing;
+using OpenTK.Graphics.OpenGL;
+
+namespace OngekiFumenEditor.Kernel.Graphics.Drawing.DefaultDrawingImpl.CircleDrawing
+{
+    internal class BatchCircleShader : Shader
+    {
+        private static BatchCircleShader _shared;
+
+        public BatchCircleShader()
+        {
+            VertexProgram = @"
+                #version 330
+
+                out vec2 pointPos;
+                out float varying_radius;
+                out vec4 varying_color;
+
+                uniform mat4 ModelViewProjection;
+                uniform vec2 uResolution;
+
+                layout(location=0) in vec4 in_color;
+                layout(location=1) in vec2 in_pos;
+                layout(location=2) in float in_radius;
+
+                void main(){
+                    varying_color = in_color;
+                    varying_radius = in_radius;
+
+	                gl_Position=ModelViewProjection * vec4(in_pos,0.0,1.0);
+                    gl_PointSize = 900.0;
+
+                    vec2 ndcPos = gl_Position.xy / gl_Position.w;
+                    pointPos    = uResolution * (ndcPos*0.5 + 0.5);
+                }
+                ";
+
+
+            FragmentProgram = @"
+                #version 330
+
+                in vec2  pointPos;
+                in vec4  varying_color;
+                in float  varying_radius;
+
+                const float threshold = 0.3;
+
+                out vec4 out_color;
+
+                void main(){
+	                float dist = distance(pointPos, gl_FragCoord.xy);
+                    if (dist > varying_radius)
+                        discard;
+
+                    float d = dist / varying_radius;
+                    vec3 color = mix(varying_color.rgb, vec3(0.0), step(1.0-threshold, d));
+
+                    out_color = vec4(color, varying_color.a);
+                }
+                ";
+        }
+
+        public static BatchCircleShader Shared => _shared ??= _createShared();
+
+        private static BatchCircleShader _createShared()
+        {
+            var shader = new BatchCircleShader();
+            shader.Compile();
+            var error = GL.GetError();
+            if (error != ErrorCode.NoError)
+                throw new Exception($"Create shared BatchCircleShader object failed ,OGL Error : {error}");
+            return shader;
+        }
+    }
+}
---
+++ OngekiFumenEditor/Kernel/Graphics/ICircleDrawing.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditor.Kernel.Graphics
+{
+    public interface ICircleDrawing : IDrawing
+    {
+        void Begin(IDrawingContext target);
+        void Post(Vector2 point,Vector4 color,bool isSolid, float radius);
+        void End();
+    }
+}
