41d50f9015fc8d42c193f8e6fdbe4ced2cc3244f
MikiraSora <mikirasora0409@126.com>
2023-07-28 12:13:49 +0800

    add IJacketGenerator

---
+++ OngekiFumenEditor/Modules/OptionGeneratorTools/IJacketGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Gemini.Framework;
+using OngekiFumenEditor.Base;
+using OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels;
+using OngekiFumenEditor.Modules.OptionGeneratorTools.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditor.Modules.FumenConverter
+{
+    public interface IJacketGenerator : IWindow
+    {
+        Task<bool> Generate(JacketGenerateOption option);
+    }
+}
---
+++ OngekiFumenEditor/Modules/OptionGeneratorTools/Models/JacketGenerateOption.cs
@@ -0,0 +1,60 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngekiFumenEditor.Modules.OptionGeneratorTools.Models
+{
+    public class JacketGenerateOption : PropertyChangedBase
+    {
+        private int musicId;
+        public int MusicId
+        {
+            get => musicId; set => Set(ref musicId, value);
+        }
+
+        private string outputAssetbundleFilePath;
+        public string OutputAssetbundleFilePath
+        {
+            get => outputAssetbundleFilePath; set => Set(ref outputAssetbundleFilePath, value);
+        }
+
+        private string outputImageFilePath;
+        public string OutputImageFilePath
+        {
+            get => outputImageFilePath; set => Set(ref outputImageFilePath, value);
+        }
+
+        private int width;
+        public int Width
+        {
+            get => width; set => Set(ref width, value);
+        }
+
+        private int height;
+        public int Height
+        {
+            get => height; set => Set(ref height, value);
+        }
+
+        private int widthSmall;
+        public int WidthSmall
+        {
+            get => widthSmall; set => Set(ref widthSmall, value);
+        }
+
+        private int heightSmall;
+        public int HeightSmall
+        {
+            get => heightSmall; set => Set(ref heightSmall, value);
+        }
+
+        private bool updateAssetBytesFile;
+        public bool UpdateAssetBytesFile
+        {
+            get => updateAssetBytesFile; set => Set(ref updateAssetBytesFile, value);
+        }
+    }
+}
