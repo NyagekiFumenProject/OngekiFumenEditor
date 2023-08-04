1ef1acf057d9f603e360b886a5d775fdb9ede0f8
MikiraSora <mikirasora0409@126.com>
2023-08-05 01:32:29 +0800

    move IObjectPropertyAccessProxy

---
+++ OngekiFumenEditor/UI/Controls/ObjectInspector/UIGenerator/IObjectPropertyAccessProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace OngekiFumenEditor.UI.Controls.ObjectInspector.UIGenerator
+{
+    public interface IObjectPropertyAccessProxy : INotifyPropertyChanged, IDisposable
+    {
+        PropertyInfo PropertyInfo { get; }
+        object ProxyValue { get; set; }
+
+        string DisplayPropertyName { get; }
+        string DisplayPropertyTipText { get; }
+    }
+}
--- OngekiFumenEditor/UI/Controls/ObjectInspector/UIGenerator/PropertyInfoWrapper.cs
+++ OngekiFumenEditor/UI/Controls/ObjectInspector/UIGenerator/PropertyInfoWrapper.cs
@@ -7,15 +7,6 @@ using System.Reflection;
 
 namespace OngekiFumenEditor.UI.Controls.ObjectInspector.UIGenerator
 {
-    public interface IObjectPropertyAccessProxy : INotifyPropertyChanged, IDisposable
-    {
-        PropertyInfo PropertyInfo { get; }
-        object ProxyValue { get; set; }
-
-        string DisplayPropertyName { get; }
-        string DisplayPropertyTipText { get; }
-    }
-
     public class PropertyInfoWrapper : PropertyChangedBase, IObjectPropertyAccessProxy
     {
         public PropertyInfo PropertyInfo { get; private set; }
