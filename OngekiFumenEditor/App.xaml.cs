d98bd7e0ce1b8107e7ca4c82a695eeb3447c1dff
MikiraSora <mikirasora0409@126.com>
2023-09-16 03:54:54 +0800

    force relocate CurrentDirectory to program folder.

--- OngekiFumenEditor/App.xaml.cs
+++ OngekiFumenEditor/App.xaml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,10 @@ namespace OngekiFumenEditor
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // 设置工作目录为执行文件所在的目录
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+        }
     }
 }
