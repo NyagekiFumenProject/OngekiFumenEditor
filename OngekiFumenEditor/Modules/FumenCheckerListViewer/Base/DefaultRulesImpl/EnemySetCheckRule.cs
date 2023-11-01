3bcb12c8b993bb16ee2877febcdf4018fde5d609
MikiraSora <mikirasora0409@126.com>
2023-11-02 07:42:15 +0800

    add SoflanCheckRule and EnemySetCheckRule

---
+++ OngekiFumenEditor/Modules/FumenCheckerListViewer/Base/DefaultRulesImpl/EnemySetCheckRule.cs
@@ -0,0 +1,29 @@
+﻿using AngleSharp.Css;
+using OngekiFumenEditor.Base;
+using OngekiFumenEditor.Modules.FumenCheckerListViewer.Base.DefaultNavigateBehaviorImpl;
+using OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using static OngekiFumenEditor.Base.OngekiObjects.EnemySet;
+
+namespace OngekiFumenEditor.Modules.FumenCheckerListViewer.Base.DefaultRulesImpl
+{
+	[Export(typeof(IFumenCheckRule))]
+	internal class EnemySetCheckRule : IFumenCheckRule
+	{
+		public IEnumerable<ICheckResult> CheckRule(OngekiFumen fumen, FumenVisualEditorViewModel fumenHostViewModel)
+		{
+			if (!fumen.EnemySets.Any(x => x.TagTblValue == WaveChangeConst.Boss))
+			{
+				yield return new CommonCheckResult()
+				{
+					Description = $"缺少Boss的EnemySet物件,建议放置一个来钦定boss出现的时机",
+					LocationDescription = string.Empty,
+					RuleName = "MissingBossEnemySet",
+					Severity = RuleSeverity.Suggest
+				};
+			}
+		}
+	}
+}
---
+++ OngekiFumenEditor/Modules/FumenCheckerListViewer/Base/DefaultRulesImpl/SoflanCheckRule.cs
@@ -0,0 +1,48 @@
+﻿using AngleSharp.Css;
+using OngekiFumenEditor.Base;
+using OngekiFumenEditor.Base.OngekiObjects;
+using OngekiFumenEditor.Modules.FumenCheckerListViewer.Base.DefaultNavigateBehaviorImpl;
+using OngekiFumenEditor.Modules.FumenVisualEditor.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using static OngekiFumenEditor.Base.OngekiObjects.EnemySet;
+
+namespace OngekiFumenEditor.Modules.FumenCheckerListViewer.Base.DefaultRulesImpl
+{
+	[Export(typeof(IFumenCheckRule))]
+	internal class SoflanCheckRule : IFumenCheckRule
+	{
+		public IEnumerable<ICheckResult> CheckRule(OngekiFumen fumen, FumenVisualEditorViewModel fumenHostViewModel)
+		{
+			bool IsConflict(double start1, double end1, double start2, double end2) => (start1 < end2 && start2 < end1) || (start2 < end1 && start1 < end2);
+			var conflictRecMap = new HashSet<Soflan>();
+
+
+			foreach (var cur in fumen.Soflans)
+			{
+				var affSoflans = fumen.Soflans.GetVisibleStartObjects(cur.TGrid, cur.EndTGrid).Except(new[] { cur });
+
+				foreach (var aff in affSoflans)
+				{
+					if (IsConflict(aff.TGrid.TotalUnit, aff.EndTGrid.TotalUnit, cur.TGrid.TotalUnit, cur.EndTGrid.TotalUnit))
+					{
+						var r = new[] { aff, cur }.MaxBy(x => x.Id);
+						if (conflictRecMap.Contains(r))
+							continue;
+						conflictRecMap.Add(r);
+
+						yield return new CommonCheckResult()
+						{
+							Description = $"发现Soflan作用时效冲突:{cur}和{aff}",
+							LocationDescription = $"({cur.TGrid},{cur.EndTGrid}) - ({aff.TGrid},{aff.EndTGrid})",
+							NavigateBehavior = new NavigateToObjectBehavior(r),
+							RuleName = "SoflanConflict",
+							Severity = RuleSeverity.Error
+						};
+					}
+				}
+			}
+		}
+	}
+}
