828ff60d328cfa23e4801cbe704740c9bf03b52e
MikiraSora <mikirasora0409@126.com>
2024-03-07 11:49:51 +0800

    fix

--- OngekiFumenEditor/Parser/DefaultImpl/Nyageki/CommandImpl/Objects/BulletPalleteCommandParser.cs
+++ OngekiFumenEditor/Parser/DefaultImpl/Nyageki/CommandImpl/Objects/BulletPalleteCommandParser.cs
@@ -7,28 +7,32 @@ using static OngekiFumenEditor.Base.OngekiObjects.BulletPallete;
 
 namespace OngekiFumenEditor.Parser.DefaultImpl.Nyageki.CommandImpl.Objects
 {
-	[Export(typeof(INyagekiCommandParser))]
-	public class BulletPalleteCommandParser : INyagekiCommandParser
-	{
-		public string CommandName => "BulletPallete";
+    [Export(typeof(INyagekiCommandParser))]
+    public class BulletPalleteCommandParser : INyagekiCommandParser
+    {
+        public string CommandName => "BulletPallete";
 
-		public void ParseAndApply(OngekiFumen fumen, string[] seg)
-		{
-			var bpl = new BulletPallete();
+        public void ParseAndApply(OngekiFumen fumen, string[] seg)
+        {
+            var bpl = new BulletPallete();
 
-			var data = seg[1].Split(":");
-			using var d = data[1].GetValuesMapWithDisposable(out var map);
+            var data = seg[1].Split(":");
+            using var d = data[1].GetValuesMapWithDisposable(out var map);
 
-			bpl.StrID = data[0].Trim();
-			bpl.ShooterValue = Enum.Parse<Shooter>(map["Shooter"]);
-			bpl.TargetValue = Enum.Parse<Target>(map["Target"]);
-			bpl.SizeValue = Enum.Parse<BulletSize>(map["Size"]);
-			bpl.TypeValue = Enum.Parse<BulletType>(map["Type"]);
-			bpl.Speed = float.Parse(map["Speed"]);
+            bpl.StrID = data[0].Trim();
+            bpl.ShooterValue = Enum.Parse<Shooter>(map["Shooter"]);
+            bpl.TargetValue = Enum.Parse<Target>(map["Target"]);
+            bpl.SizeValue = Enum.Parse<BulletSize>(map["Size"]);
+            bpl.TypeValue = Enum.Parse<BulletType>(map["Type"]);
+            bpl.Speed = float.Parse(map["Speed"]);
             bpl.RandomOffsetRange = int.Parse(map["RandomOffsetRange"]);
             bpl.PlaceOffset = int.Parse(map["PlaceOffset"]);
 
-			fumen.AddObject(bpl);
-		}
-	}
+            //兼容老铺面
+            if (map.TryGetValue("RandomOffsetRange", out var r))
+                bpl.RandomOffsetRange = int.Parse(r);
+
+            fumen.AddObject(bpl);
+        }
+    }
 }
