7fad74a166c9ea85263336a0632ae179ba479ef4
MikiraSora <mikirasora0409@126.com>
2024-08-29 12:46:50 +0800

    ColorId is comparable

--- OngekiFumenEditor/Base/OngekiObjects/ColorId.cs
+++ OngekiFumenEditor/Base/OngekiObjects/ColorId.cs
@@ -1,13 +1,26 @@
-﻿using System.Windows.Media;
+﻿using System.Numerics;
+using System.Windows.Media;
 
-namespace OngekiFumenEditor.Base.OngekiObjects
+namespace OngekiFumenEditor.Base.OngekiObjects;
+
+public struct ColorId : IEqualityOperators<ColorId, ColorId, bool>
 {
-	public struct ColorId
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public Color Color { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Color Color { get; set; }
 
-		public override string ToString() => $"{Id} {Name}";
-	}
+    public override string ToString()
+    {
+        return $"{Id} {Name}";
+    }
+
+    public static bool operator ==(ColorId left, ColorId right)
+    {
+        return left.Id == right.Id;
+    }
+
+    public static bool operator !=(ColorId left, ColorId right)
+    {
+        return !(left == right);
+    }
 }
