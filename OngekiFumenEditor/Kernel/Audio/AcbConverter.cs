592bbd7a12e9dda775c3d3d66b30e095c6070f77
MikiraSora <mikirasora0409@126.com>
2024-09-03 00:00:05 +0800

    refactory AcbConverter

--- OngekiFumenEditor/Kernel/Audio/AcbConverter.cs
+++ OngekiFumenEditor/Kernel/Audio/AcbConverter.cs
@@ -1,122 +1,113 @@
-﻿using DereTore.Exchange.Archive.ACB;
-using DereTore.Exchange.Audio.HCA;
-using OngekiFumenEditor.Utils;
-using System;
+﻿using System;
 using System.Buffers;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
+using DereTore.Exchange.Archive.ACB;
+using DereTore.Exchange.Audio.HCA;
+using OngekiFumenEditor.Utils;
 
-namespace OngekiFumenEditor.Kernel.Audio
+namespace OngekiFumenEditor.Kernel.Audio;
+
+public static class AcbConverter
 {
-    public static class AcbConverter
+    private static readonly object locker = new();
+
+    private static async Task ProcessAllBinaries(uint acbFormatVersion, string extractFilePath, Afs2Archive archive,
+        Stream dataStream)
     {
-        private static Mutex mutex = new Mutex();
-
-        private static void ProcessAllBinaries(uint acbFormatVersion, string extractFilePath, Afs2Archive archive, Stream dataStream)
+        async Task DecodeHca(Stream hcaDataStream, Stream waveStream, DecodeParams decodeParams)
         {
-            void DecodeHca(Stream hcaDataStream, Stream waveStream, DecodeParams decodeParams)
+            using var hcaStream = new OneWayHcaAudioStream(hcaDataStream, decodeParams, true);
+            var buffer = ArrayPool<byte>.Shared.Rent(1_024_000);
+            var read = 1;
+
+            while (read > 0)
             {
-                using var hcaStream = new OneWayHcaAudioStream(hcaDataStream, decodeParams, true);
-                var buffer = ArrayPool<byte>.Shared.Rent(1_024_000);
-                var read = 1;
+                read = await hcaStream.ReadAsync(buffer, 0, buffer.Length);
 
-                while (read > 0)
-                {
-                    read = hcaStream.Read(buffer, 0, buffer.Length);
-
-                    if (read > 0)
-                    {
-                        waveStream.Write(buffer, 0, read);
-                    }
-                }
-
-                ArrayPool<byte>.Shared.Return(buffer);
+                if (read > 0)
+                    await waveStream.WriteAsync(buffer, 0, read);
             }
 
-
-            foreach (var entry in archive.Files)
-            {
-                var record = entry.Value;
-                var len = (int)record.FileLength;
-                var buffer = ArrayPool<byte>.Shared.Rent(len);
-                dataStream.Seek(record.FileOffsetAligned, SeekOrigin.Begin);
-                var read = dataStream.Read(buffer, 0, len);
-                var fileData = new MemoryStream(buffer, 0, read);
-
-                if (HcaReader.IsHcaStream(fileData))
-                {
-                    Log.LogDebug(string.Format("Processing {0} AFS: #{1} (offset={2} size={3})...   ", acbFormatVersion, record.CueId, record.FileOffsetAligned, record.FileLength));
-
-                    try
-                    {
-                        using var fs = File.Open(extractFilePath, FileMode.Create, FileAccess.Write, FileShare.Write);
-                        DecodeHca(fileData, fs, DecodeParams.Default);
-
-                        Log.LogDebug("decoded");
-                    }
-                    catch (Exception ex)
-                    {
-                        if (File.Exists(extractFilePath))
-                            File.Delete(extractFilePath);
-
-                        Log.LogDebug(ex.ToString());
-
-                        if (ex.InnerException != null)
-                        {
-                            Log.LogDebug("Details:");
-                            Log.LogDebug(ex.InnerException.ToString());
-                        }
-                    }
-                }
-                else
-                {
-                    Log.LogDebug("skipped (not HCA)");
-                }
-                ArrayPool<byte>.Shared.Return(buffer);
-            }
+            ArrayPool<byte>.Shared.Return(buffer);
         }
 
-        public static Task<string> ConvertAcbFileToWavFile(string filePath)
+
+        foreach (var entry in archive.Files)
         {
-            return Task.Run(() =>
+            var record = entry.Value;
+            var len = (int) record.FileLength;
+            var buffer = ArrayPool<byte>.Shared.Rent(len);
+            dataStream.Seek(record.FileOffsetAligned, SeekOrigin.Begin);
+            var read = dataStream.Read(buffer, 0, len);
+            var fileData = new MemoryStream(buffer, 0, read);
+
+            if (HcaReader.IsHcaStream(fileData))
             {
-                mutex.WaitOne();
-
-                Log.LogInfo($"Extract .acb to .wav and load the later , acb file path : {filePath}");
-
-                using var acb = AcbFile.FromFile(filePath);
-                var tempFolder = Path.Combine(Path.GetTempPath(), "ParseAndDecodeACBFile");
-                Directory.CreateDirectory(tempFolder);
-
-                var formatVersion = acb.FormatVersion;
-                var awb = acb.InternalAwb ?? acb.ExternalAwb;
-                var tempAwbFilePath = Path.Combine(tempFolder, Path.GetFileNameWithoutExtension(filePath) + ".wav");
-
-                if (File.Exists(tempAwbFilePath))
-                {
-                    Log.LogInfo($"use cache file: {tempAwbFilePath}");
-                    mutex.ReleaseMutex();
-                    return tempAwbFilePath;
-                }
+                Log.LogDebug(string.Format("Processing {0} AFS: #{1} (offset={2} size={3})...   ", acbFormatVersion,
+                    record.CueId, record.FileOffsetAligned, record.FileLength));
 
                 try
                 {
-                    using var awbStream = awb == acb.InternalAwb ? acb.Stream : File.OpenRead(awb.FileName);
-                    ProcessAllBinaries(acb.FormatVersion, tempAwbFilePath, awb, awbStream);
-                    Log.LogInfo($"generate new: {tempAwbFilePath}");
-                    return tempAwbFilePath;
+                    using var fs = File.Open(extractFilePath, FileMode.Create, FileAccess.Write, FileShare.Write);
+                    await DecodeHca(fileData, fs, DecodeParams.Default);
+
+                    Log.LogDebug("decoded");
                 }
-                catch (Exception e)
+                catch (Exception ex)
                 {
-                    Log.LogError($"Load acb file failed : {e.Message}");
-                    return null;
+                    if (File.Exists(extractFilePath))
+                        File.Delete(extractFilePath);
+
+                    Log.LogDebug(ex.ToString());
+
+                    if (ex.InnerException != null)
+                    {
+                        Log.LogDebug("Details:");
+                        Log.LogDebug(ex.InnerException.ToString());
+                    }
                 }
-                finally
-                {
-                    mutex.ReleaseMutex();
-                }
-            });
+            }
+            else
+            {
+                Log.LogDebug("skipped (not HCA)");
+            }
+
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+    }
+
+    public static async Task<string> ConvertAcbFileToWavFile(string filePath)
+    {
+        string tempAwbFilePath;
+        lock (locker)
+        {
+            var tempFolder = TempFileHelper.GetTempFolderPath(prefix: "decodeAcbFiles", random: false);
+            tempAwbFilePath = Path.Combine(tempFolder, Path.GetFileNameWithoutExtension(filePath) + ".wav");
+            Log.LogInfo($"Extract .acb to .wav and load the later , acb file path : {tempAwbFilePath}");
+            
+            if (File.Exists(tempAwbFilePath))
+            {
+                Log.LogInfo($"use cache file: {tempAwbFilePath}");
+                return tempAwbFilePath;
+            }
+        }
+
+        try
+        {
+            using var acb = AcbFile.FromFile(filePath);
+            var formatVersion = acb.FormatVersion;
+            var awb = acb.InternalAwb ?? acb.ExternalAwb;
+
+            using var awbStream = awb == acb.InternalAwb ? acb.Stream : File.OpenRead(awb.FileName);
+            await ProcessAllBinaries(acb.FormatVersion, tempAwbFilePath, awb, awbStream);
+            Log.LogInfo($"generate new: {tempAwbFilePath}");
+            return tempAwbFilePath;
+        }
+        catch (Exception e)
+        {
+            Log.LogError($"Load acb file failed : {e.Message}");
+            return null;
         }
     }
 }
