30969ba98f06236caeecde9d68f6341a292b3f7c
MikiraSora <mikirasora0409@126.com>
2024-09-13 00:02:35 +0800

    fix crash

--- OngekiFumenEditor/Utils/ImageLoader.cs
+++ OngekiFumenEditor/Utils/ImageLoader.cs
@@ -18,171 +18,173 @@ using System.Windows.Markup;
 
 namespace OngekiFumenEditor.Utils
 {
-	[Export(typeof(ImageLoader))]
-	public class ImageLoader
-	{
-		private readonly byte[] abMagic = "UnityFS"u8.ToArray();
+    [Export(typeof(ImageLoader))]
+    public class ImageLoader
+    {
+        private readonly byte[] abMagic = "UnityFS"u8.ToArray();
 
-		private const int ParallelCount = 2;
-		private readonly ConcurrentDictionary<string, WeakReference<byte[]>> cacheMap = new();
-		private readonly ConcurrentStack<LoadTask> tasks = new();
+        private const int ParallelCount = 2;
+        private readonly ConcurrentDictionary<string, WeakReference<byte[]>> cacheMap = new();
+        private readonly ConcurrentStack<LoadTask> tasks = new();
 
-		public ImageLoader()
-		{
-			Task.Run(PrcessQueue);
-		}
+        public ImageLoader()
+        {
+            Task.Run(PrcessQueue);
+        }
 
-		public Task<byte[]> LoadImage(string url, CancellationToken cancellationToken)
-		{
-			var taskCompleteSource = new TaskCompletionSource<byte[]>();
-			tasks.Push(new LoadTask(taskCompleteSource, url));
-			return taskCompleteSource.Task;
-		}
+        public Task<byte[]> LoadImage(string url, CancellationToken cancellationToken)
+        {
+            var taskCompleteSource = new TaskCompletionSource<byte[]>();
+            tasks.Push(new LoadTask(taskCompleteSource, url));
+            return taskCompleteSource.Task;
+        }
 
-		private async void PrcessQueue()
-		{
-			var currentRunningTasks = new List<Task>(ParallelCount);
-			while (true)
-			{
-				for (var i = 0; i < ParallelCount; i++)
-					if (tasks.TryPop(out var task))
-						currentRunningTasks.Add(Task.Run(() =>
-						{
-							var url = task.url;
-							var taskSource = task.TaskSource;
+        private async void PrcessQueue()
+        {
+            var currentRunningTasks = new List<Task>(ParallelCount);
+            while (true)
+            {
+                for (var i = 0; i < ParallelCount; i++)
+                    if (tasks.TryPop(out var task))
+                        currentRunningTasks.Add(Task.Run(() =>
+                        {
+                            var url = task.url;
+                            var taskSource = task.TaskSource;
 
-							return ProcessTask(url, taskSource);
-						}));
+                            return ProcessTask(url, taskSource);
+                        }));
 
-				if (currentRunningTasks.Count > 0)
-					await Task.WhenAll(currentRunningTasks);
-				currentRunningTasks.Clear();
-				await Task.Delay(0);
-			}
-		}
+                if (currentRunningTasks.Count > 0)
+                    await Task.WhenAll(currentRunningTasks);
+                currentRunningTasks.Clear();
+                await Task.Delay(0);
+            }
+        }
 
-		private async ValueTask ProcessTask(string path, TaskCompletionSource<byte[]> taskSource)
-		{
-			using var md5 = MD5.Create();
-			var hash = Convert.ToHexString(md5.ComputeHash(Encoding.UTF8.GetBytes(path)));
+        private async ValueTask ProcessTask(string path, TaskCompletionSource<byte[]> taskSource)
+        {
+            using var md5 = MD5.Create();
+            var hash = Convert.ToHexString(md5.ComputeHash(Encoding.UTF8.GetBytes(path)));
 
-			var isNetworkLoad = path.StartsWith("http", StringComparison.InvariantCultureIgnoreCase);
+            var isNetworkLoad = path.StartsWith("http", StringComparison.InvariantCultureIgnoreCase);
 
-			var data = await LoadFromInMemory(path);
-			if (data != null)
-			{
-				taskSource.SetResult(data);
-				return;
-			}
+            var data = await LoadFromInMemory(path);
+            if (data != null)
+            {
+                taskSource.SetResult(data);
+                return;
+            }
 
-			if (isNetworkLoad)
-			{
-				data = await LoadCache(hash);
-				if (data != null)
-				{
-					taskSource.SetResult(data);
-					return;
-				}
-			}
+            if (isNetworkLoad)
+            {
+                data = await LoadCache(hash);
+                if (data != null)
+                {
+                    taskSource.SetResult(data);
+                    return;
+                }
+            }
 
-			data = await Load(path, isNetworkLoad);
-			if (data == null)
-			{
-				taskSource.SetResult(null);
-				return;
-			}
-			taskSource.SetResult(data);
+            data = await Load(path, isNetworkLoad);
+            if (data == null)
+            {
+                taskSource.SetResult(null);
+                return;
+            }
+            taskSource.SetResult(data);
 
-			if (isNetworkLoad)
-				await SaveCache(hash, data);
-			await SaveFromInMemory(hash, data);
-		}
+            if (isNetworkLoad)
+                await SaveCache(hash, data);
+            await SaveFromInMemory(hash, data);
+        }
 
-		private string GetCacheFile(string hash) => TempFileHelper.GetTempFilePath("images", hash, "img.cache", false);
+        private string GetCacheFile(string hash) => TempFileHelper.GetTempFilePath("images", hash, "img.cache", false);
 
-		private async Task SaveCache(string hash, byte[] data)
-		{
-			var filePath = GetCacheFile(hash);
-			await File.WriteAllBytesAsync(filePath, data);
-		}
+        private async Task SaveCache(string hash, byte[] data)
+        {
+            var filePath = GetCacheFile(hash);
+            await File.WriteAllBytesAsync(filePath, data);
+        }
 
-		private async Task<byte[]> LoadCache(string hash)
-		{
-			var filePath = GetCacheFile(hash);
-			if (File.Exists(filePath))
-				return await File.ReadAllBytesAsync(filePath);
-			return null;
-		}
+        private async Task<byte[]> LoadCache(string hash)
+        {
+            var filePath = GetCacheFile(hash);
+            if (File.Exists(filePath))
+                return await File.ReadAllBytesAsync(filePath);
+            return null;
+        }
 
-		private ValueTask SaveFromInMemory(string hash, byte[] data)
-		{
-			cacheMap[hash] = new WeakReference<byte[]>(data);
-			return ValueTask.CompletedTask;
-		}
+        private ValueTask SaveFromInMemory(string hash, byte[] data)
+        {
+            cacheMap[hash] = new WeakReference<byte[]>(data);
+            return ValueTask.CompletedTask;
+        }
 
-		private ValueTask<byte[]> LoadFromInMemory(string hash)
-		{
-			if (cacheMap.TryGetValue(hash, out var weakReference))
-				if (weakReference.TryGetTarget(out var data))
-					return ValueTask.FromResult(data);
+        private ValueTask<byte[]> LoadFromInMemory(string hash)
+        {
+            if (cacheMap.TryGetValue(hash, out var weakReference))
+                if (weakReference.TryGetTarget(out var data))
+                    return ValueTask.FromResult(data);
 
-			return ValueTask.FromResult(default(byte[]));
-		}
+            return ValueTask.FromResult(default(byte[]));
+        }
 
-		private async ValueTask<byte[]> Load(string path, bool isNetworkLoad)
-		{
-			async ValueTask<byte[]> GetRaw()
-			{
-				try
-				{
-					if (isNetworkLoad)
-					{
-						using var httpClient = new HttpClient();
-						return await httpClient.GetByteArrayAsync(path);
-					}
-					else
-					{
-						return await File.ReadAllBytesAsync(path);
-					}
-				}
-				catch (Exception e)
-				{
-					Log.LogError($"load {path} failed", e);
-					return default;
-				}
-			}
+        private async ValueTask<byte[]> Load(string path, bool isNetworkLoad)
+        {
+            async ValueTask<byte[]> GetRaw()
+            {
+                try
+                {
+                    if (isNetworkLoad)
+                    {
+                        using var httpClient = new HttpClient();
+                        return await httpClient.GetByteArrayAsync(path);
+                    }
+                    else
+                    {
+                        return await File.ReadAllBytesAsync(path);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.LogError($"load {path} failed", e);
+                    return default;
+                }
+            }
 
-			var r = await GetRaw();
+            var r = await GetRaw();
 
-			if (r.Length >= abMagic.Length)
-			{
-				var isABFile = true;
-				for (var i = 0; i < abMagic.Length; i++)
-				{
-					if (abMagic[i] != r[i])
-					{
-						isABFile = false;
-						break;
-					}
-				}
+            if (r.Length >= abMagic.Length)
+            {
+                var isABFile = true;
+                for (var i = 0; i < abMagic.Length; i++)
+                {
+                    if (abMagic[i] != r[i])
+                    {
+                        isABFile = false;
+                        break;
+                    }
+                }
 
-				if (isABFile)
-				{
-					var imgData = await JacketGenerateWrapper.GetMainImageDataAsync(r, path);
-					using var image = Image.LoadPixelData<Rgba32>(imgData.Data, imgData.Width, imgData.Height);
-					var memoryStream = new MemoryStream();
-					image.Mutate(i => i.Flip(FlipMode.Vertical));
-					image.SaveAsPng(memoryStream);
-					memoryStream.Seek(0, SeekOrigin.Begin);
+                if (isABFile)
+                {
+                    var imgData = await JacketGenerateWrapper.GetMainImageDataAsync(r, path);
+                    if (imgData is null)
+                        return default;
+                    using var image = Image.LoadPixelData<Rgba32>(imgData.Data, imgData.Width, imgData.Height);
+                    var memoryStream = new MemoryStream();
+                    image.Mutate(i => i.Flip(FlipMode.Vertical));
+                    image.SaveAsPng(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
 
-					return memoryStream.ToArray();
-				}
-			}
+                    return memoryStream.ToArray();
+                }
+            }
 
-			return r;
-		}
+            return r;
+        }
 
-		private record LoadTask(TaskCompletionSource<byte[]> TaskSource, string url);
+        private record LoadTask(TaskCompletionSource<byte[]> TaskSource, string url);
 
-	}
+    }
 }
