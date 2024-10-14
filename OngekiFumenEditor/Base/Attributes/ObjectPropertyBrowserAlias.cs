7f2f7ec178611f83db50c7a6d17bbb27a0a6365d
MikiraSora <mikirasora0409@126.com>
2024-10-14 18:38:21 +0800

    add CN I18N

--- OngekiFumenEditor/Base/Attributes/ObjectPropertyBrowserAlias.cs
+++ OngekiFumenEditor/Base/Attributes/ObjectPropertyBrowserAlias.cs
@@ -17,18 +17,20 @@ namespace OngekiFumenEditor.Base.Attributes
     [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
     public class LocalizableObjectPropertyBrowserAlias : ObjectPropertyBrowserAlias
     {
-        public LocalizableObjectPropertyBrowserAlias(string key)
+        public LocalizableObjectPropertyBrowserAlias(string resourceKey)
         {
 #if DEBUG
-            if (key == default) {
+            if (resourceKey == default)
+            {
                 throw new ArgumentException("cannot use empty string as resource key");
             }
-            if (Resources.ResourceManager.GetString(key) is null) {
-                throw new ArgumentException($"invalid resource key \"{key}\"");
+            if (Resources.ResourceManager.GetString(resourceKey) is null)
+            {
+                throw new ArgumentException($"invalid resource key \"{resourceKey}\"");
             }
 #endif
-            
-            Alias = Resources.ResourceManager.GetString(key!) ?? string.Empty;
+
+            Alias = Resources.ResourceManager.GetString(resourceKey!) ?? string.Empty;
         }
     }
 }
