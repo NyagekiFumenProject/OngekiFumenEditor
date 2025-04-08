cdcc95f51105cdad853d323f6d6051ba73eba60b
MikiraSora <mikirasora0409@126.com>
2025-04-08 23:09:37 +0800

    optimze Log

--- OngekiFumenEditor/Utils/Log.cs
+++ OngekiFumenEditor/Utils/Log.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Diagnostics;
@@ -7,8 +8,10 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Caliburn.Micro;
 using OngekiFumenEditor.Utils.Logs;
+using OngekiFumenEditor.Utils.Logs.DefaultImpls;
 using static OngekiFumenEditor.Utils.Logs.ILogOutput;
 
 namespace OngekiFumenEditor.Utils;
@@ -17,10 +20,12 @@ namespace OngekiFumenEditor.Utils;
 [PartCreationPolicy(CreationPolicy.Shared)]
 public class Log
 {
+    private record LogRecord(Severity Severity, string Message, bool NewLine, bool Time, string Prefix, string FilePath, int LineNumber);
+
     private static Log cacheInstance;
     private readonly List<ILogOutput> outputs = new();
 
-    private readonly StringBuilder sb = new(2048);
+    private static ConcurrentQueue<LogRecord> logRecordQueue = new();
 
     [ImportingConstructor]
     public Log([ImportMany] IEnumerable<ILogOutput> outputs)
@@ -49,75 +54,103 @@ public class Log
             output.WriteLog(severity, message);
     }
 
-    private string BuildLogMessage(string message, Severity severity, bool new_line, bool time, string prefix, string filePath, int lineNumber)
+    private static string BuildLogMessage(LogRecord record)
     {
+        var prefix = record.Prefix;
         if (prefix == ".ctor")
-            prefix = Path.GetFileNameWithoutExtension(filePath) + prefix;
-        prefix += $":{lineNumber}";
+            prefix = Path.GetFileNameWithoutExtension(record.FilePath) + prefix;
+        prefix += $":{record.LineNumber}";
 
-        lock (sb)
-        {
-            sb.Clear();
+        using var _d = ObjectPool.ObjectPool.GetWithUsingDisposable<StringBuilder>(out var sb, out _);
+        sb.Clear();
 
-            sb.AppendFormat("[{0} {1}:{2}]", time ? DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") : string.Empty,
-                severity.ToString().ToUpper(), Thread.CurrentThread.ManagedThreadId);
+        sb.AppendFormat("[{0} {1}:{2}]", record.Time ? DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") : string.Empty,
+            record.Severity.ToString().ToUpper(), Thread.CurrentThread.ManagedThreadId);
 
-            if (!string.IsNullOrWhiteSpace(prefix))
-                sb.AppendFormat("<{0}>", prefix);
+        if (!string.IsNullOrWhiteSpace(prefix))
+            sb.AppendFormat("<{0}>", prefix);
 
-            sb.AppendFormat(" {0}", message.TrimStart());
+        sb.AppendFormat(" {0}", record.Message.TrimStart());
 
-            if (new_line)
-                sb.AppendLine();
+        if (record.NewLine)
+            sb.AppendLine();
 
-            return sb.ToString();
-        }
+        return sb.ToString();
+    }
+
+    private static void EnqueueLogRecord(string message, Severity severity, bool new_line, bool time, string prefix, string filePath, int lineNumber)
+    {
+        logRecordQueue.Enqueue(new LogRecord(severity, message, new_line, time, prefix, filePath, lineNumber));
     }
 
     [Conditional("DEBUG")]
     public static void LogDebug(string message, bool newLine = true, bool time = true,
         [CallerMemberName] string prefix = "<Unknown>", [CallerFilePath] string filePath = default, [CallerLineNumber] int lineNumber = 0)
     {
-        var instance = Instance;
         var severity = Severity.Debug;
-        var msg = instance.BuildLogMessage(message, severity, newLine, time, prefix, filePath, lineNumber);
-        instance.Output(severity, msg);
+        EnqueueLogRecord(message, severity, newLine, time, prefix, filePath, lineNumber);
+        AwakeLogger();
+    }
+
+    private static volatile bool isRunning = false;
+
+    private static void AwakeLogger()
+    {
+        if (isRunning)
+            return;
+
+        isRunning = true;
+
+        Task.Run(() =>
+        {
+            while (logRecordQueue.TryDequeue(out var logRecord))
+            {
+                try
+                {
+                    var msg = BuildLogMessage(logRecord);
+                    Instance.Output(logRecord.Severity, msg);
+                }
+                catch (Exception e)
+                {
+#if DEBUG
+                    throw;
+#endif
+                }
+            }
+
+            isRunning = false;
+        });
     }
 
     public static void LogInfo(string message, bool newLine = true, bool time = true,
         [CallerMemberName] string prefix = "<Unknown>", [CallerFilePath] string filePath = default, [CallerLineNumber] int lineNumber = 0)
     {
-        var instance = Instance;
         var severity = Severity.Info;
-        var msg = instance.BuildLogMessage(message, severity, newLine, time, prefix, filePath, lineNumber);
-        instance.Output(severity, msg);
+        EnqueueLogRecord(message, severity, newLine, time, prefix, filePath, lineNumber);
+        AwakeLogger();
     }
 
     public static void LogWarn(string message, bool newLine = true, bool time = true,
         [CallerMemberName] string prefix = "<Unknown>", [CallerFilePath] string filePath = default, [CallerLineNumber] int lineNumber = 0)
     {
-        var instance = Instance;
         var severity = Severity.Warn;
-        var msg = instance.BuildLogMessage(message, severity, newLine, time, prefix, filePath, lineNumber);
-        instance.Output(severity, msg);
+        EnqueueLogRecord(message, severity, newLine, time, prefix, filePath, lineNumber);
+        AwakeLogger();
     }
 
     public static void LogError(string message, bool newLine = true, bool time = true,
         [CallerMemberName] string prefix = "<Unknown>", [CallerFilePath] string filePath = default, [CallerLineNumber] int lineNumber = 0)
     {
-        var instance = Instance;
         var severity = Severity.Error;
-        var msg = instance.BuildLogMessage(message, severity, newLine, time, prefix, filePath, lineNumber);
-        instance.Output(severity, msg);
+        EnqueueLogRecord(message, severity, newLine, time, prefix, filePath, lineNumber);
+        AwakeLogger();
     }
 
     public static void LogError(string message, Exception e, bool newLine = true, bool time = true,
         [CallerMemberName] string prefix = "<Unknown>", [CallerFilePath] string filePath = default, [CallerLineNumber] int lineNumber = 0)
     {
-        var instance = Instance;
         var severity = Severity.Error;
-        var msg = instance.BuildLogMessage($"{message}\nContains exception:{e.Message}\n{e.StackTrace}", severity,
-            newLine, time, prefix, filePath, lineNumber);
-        instance.Output(severity, msg);
+        EnqueueLogRecord(message, severity, newLine, time, prefix, filePath, lineNumber);
+        AwakeLogger();
     }
 }
