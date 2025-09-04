635ce2671c7fb5c4b0dde785a9a921cdfeb1f905
MikiraSora <mikirasora0409@126.com>
2025-09-05 06:43:24 +0800

    add OngekiFumenEditor.CommandLine for commandline

---
+++ OngekiFumenEditor.CommandLine/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OngekiFumenEditor.CommandLine
+{
+    internal class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            var app = new App(false);
+            app.InitializeComponent();
+
+            return app.Run();
+        }
+    }
+}
